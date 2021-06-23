d1ef646ad4c5572910154dc854f162e69aabce78
alexeyrozhkov <alex31inq@gmail.com>
2021-06-23 18:01:25 +0300

    create app

---
+++ redux-course/src/containers/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
+
+class App extends React.Component {
+  render() {
+    const {user, age , surname} = this.props;
+    return <div>
+      <p>Привет из App, { user } {surname}!</p>
+      <p>Тебе уже {age} ?</p>
+    </div>
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ redux-course/src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    user: 'Unknown User',
+    surname: 'Реактов',
+    age: 27
+};
+  
+export default function userstate(state = initialState) {
+    return state;
+}
